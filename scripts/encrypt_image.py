442781f61fe610c63ff991ca1f824b124380d49f
zhangqifei1998 <124134999+zhangqifei1998@users.noreply.github.com>
2024-01-09 11:43:13 +0800

    Update encrypt_image.py

--- scripts/encrypt_image.py
+++ scripts/encrypt_image.py
@@ -16,7 +16,7 @@ from fastapi import FastAPI, Request, Response
 import sys
 
 repo_dir = md_scripts.basedir()
-password = getattr(shared.cmd_opts, 'encrypt_pass', '1')
+password = getattr(shared.cmd_opts, 'encrypt_pass2', '1')
 
 if PILImage.Image.__name__ != 'EncryptedImage':
     super_open = PILImage.open
